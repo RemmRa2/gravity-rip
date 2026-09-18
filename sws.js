c94ed45b6939a67009cb13ff65d1d06331a14936
RemmRa2 <rayan.remmouche2@ped.csmb.qc.ca>
2026-09-18 09:00:00 -0400

    Update sws.js

--- sws.js
+++ sws.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./sw.js')
+        navigator.serviceWorker.register('./sw.js?v=1')
             
             .catch((err) => console.log("Échec du SW :", err));
     });
